cd09ed4a983e4279518da4fd1e6dc4eb64d34112
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-08-16 23:27:14 -0500

    aug16-2021

--- 7segments/segment7.v
+++ 7segments/segment7.v
@@ -1,25 +1,30 @@
 module segment7(
-     input [3:0] bcd,
-     output[6:0] out
+     input A, B, C, D,
+     output a,b,c,d,e,f,g
     );
 
-    reg [6:0] out;
+    wire[3:0] Din;
+    assign Din = {A,B,C,D};
+    //wire[6:0] Dout;
+    reg[6:0] Dout;
 
     always @(*)
     begin
-        case (bcd)
-            default : out = 7'b1111111; 
-            0 : out = 7'b1111110;
-            1 : out = 7'b0110000;
-            2 : out = 7'b1101101;
-            3 : out = 7'b1111001;
-            4 : out = 7'b0110011;
-            5 : out = 7'b1011011;
-            6 : out = 7'b1011111;
-            7 : out = 7'b1110000;
-            8 : out = 7'b1111111;
-            9 : out = 7'b1111011;
+        case (Din)
+            default : Dout = 7'b100_1111; 
+            0 : Dout = 7'b111_1110;
+            1 : Dout = 7'b011_0000;
+            2 : Dout = 7'b110_1101;
+            3 : Dout = 7'b111_1001;
+            4 : Dout = 7'b011_0011;
+            5 : Dout = 7'b101_1011;
+            6 : Dout = 7'b101_1111;
+            7 : Dout = 7'b111_0000;
+            8 : Dout = 7'b111_1111;
+            9 : Dout = 7'b111_1011;
         endcase
     end
+
+    assign {a,b,c,d,e,f,g} = Dout;
     
 endmodule
