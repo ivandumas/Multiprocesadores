a1b002db635e1cc5e6b0a19dc0e22d8f35c1f5d1
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-08-16 20:42:24 -0500

    Verilog 7 segment decoder

--- 7segments/segment7.v
+++ 7segments/segment7.v
@@ -1,24 +1,24 @@
 module segment7(
      input [3:0] bcd,
-     output[6:0] seg
+     output[6:0] out
     );
 
-    reg [6:0] seg;
+    reg [6:0] out;
 
     always @(*)
     begin
-        case (bcd) //case statement
-            default : seg = 7'b1111111; 
-            0 : seg = 7'b1111110;
-            1 : seg = 7'b0110000;
-            2 : seg = 7'b1101101;
-            3 : seg = 7'b1111001;
-            4 : seg = 7'b0110011;
-            5 : seg = 7'b1011011;
-            6 : seg = 7'b1011111;
-            7 : seg = 7'b1110000;
-            8 : seg = 7'b1111111;
-            9 : seg = 7'b1111011;
+        case (bcd)
+            default : out = 7'b1111111; 
+            0 : out = 7'b1111110;
+            1 : out = 7'b0110000;
+            2 : out = 7'b1101101;
+            3 : out = 7'b1111001;
+            4 : out = 7'b0110011;
+            5 : out = 7'b1011011;
+            6 : out = 7'b1011111;
+            7 : out = 7'b1110000;
+            8 : out = 7'b1111111;
+            9 : out = 7'b1111011;
         endcase
     end
     
